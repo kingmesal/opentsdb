e2facb6384845cb0b26f5ff074ea4c153b033a39
haden dude <haden.dude@gmail.com>
2014-11-13 16:30:22 -0800

    more tests for BatchedDataPoints

--- test/core/TestBatchedDataPoints.java
+++ test/core/TestBatchedDataPoints.java
@@ -1,3 +1,15 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2014  The OpenTSDB Authors.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or (at your
+// option) any later version.  This program is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+// General Public License for more details.  You should have received a copy
+// of the GNU Lesser General Public License along with this program.  If not,
+// see <http://www.gnu.org/licenses/>.
 package net.opentsdb.core;
 
 import static org.junit.Assert.assertEquals;
@@ -26,8 +38,11 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.stumbleupon.async.Deferred;
+
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({TSDB.class, Config.class, UniqueId.class, HBaseClient.class, IncomingDataPoints.class})
+@PrepareForTest({ TSDB.class, Config.class, UniqueId.class, HBaseClient.class,
+    IncomingDataPoints.class })
 public class TestBatchedDataPoints {
   private static Config config;
   private static TSDB tsdb = null;
@@ -35,11 +50,13 @@ public class TestBatchedDataPoints {
   private UniqueId metrics = mock(UniqueId.class);
   private UniqueId tag_names = mock(UniqueId.class);
   private UniqueId tag_values = mock(UniqueId.class);
+  private BatchedDataPoints bdp = null;
 
+  @SuppressWarnings("unchecked")
   @Before
   public void before() throws Exception {
     PowerMockito.whenNew(HBaseClient.class)
-    .withArguments(anyString(), anyString()).thenReturn(client);
+        .withArguments(anyString(), anyString()).thenReturn(client);
     config = new Config(false);
     tsdb = new TSDB(config);
 
@@ -47,57 +64,39 @@ public class TestBatchedDataPoints {
     Field met = tsdb.getClass().getDeclaredField("metrics");
     met.setAccessible(true);
     met.set(tsdb, metrics);
-    
+
     Field tagk = tsdb.getClass().getDeclaredField("tag_names");
     tagk.setAccessible(true);
     tagk.set(tsdb, tag_names);
-    
+
     Field tagv = tsdb.getClass().getDeclaredField("tag_values");
     tagv.setAccessible(true);
     tagv.set(tsdb, tag_values);
-    
-    // mock UniqueId
+
+    when(metrics.width()).thenReturn((short) 3);
+    when(tag_names.width()).thenReturn((short) 3);
+    when(tag_values.width()).thenReturn((short) 3);
+
     when(metrics.getId("foo")).thenReturn(new byte[] { 0, 0, 1 });
-    when(metrics.getName(new byte[] { 0, 0, 1 })).thenReturn("foo");
+    when(metrics.getNameAsync(new byte[] { 0, 0, 1 })).thenReturn(
+        Deferred.fromResult("foo"));
 
-    when(tag_names.getId("host")).thenReturn(new byte[] { 0, 0, 1 });
-    when(tag_names.getName(new byte[] { 0, 0, 1 })).thenReturn("host");
-    when(tag_names.getOrCreateId("host")).thenReturn(new byte[] { 0, 0, 1 });
+    PowerMockito.mockStatic(IncomingDataPoints.class);
+    final byte[] row = new byte[] { 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1 };
+    PowerMockito.doAnswer(new Answer<byte[]>() {
+      public byte[] answer(final InvocationOnMock unused) throws Exception {
+        return row;
+      }
+    }).when(IncomingDataPoints.class, "rowKeyTemplate", (TSDB) any(),
+        anyString(), (Map<String, String>) any());
 
-    when(tag_values.getId("web01")).thenReturn(new byte[] { 0, 0, 1 });
-    when(tag_values.getName(new byte[] { 0, 0, 1 })).thenReturn("web01");
-    when(tag_values.getOrCreateId("web01")).thenReturn(new byte[] { 0, 0, 1 });
-    
-    when(metrics.width()).thenReturn((short)1);
-    when(tag_names.width()).thenReturn((short)1);
-    when(tag_values.width()).thenReturn((short)1);
-  }
-
-  /**
-   * Configures storage for the addPoint() tests to validate that we're storing
-   * data points correctly.
-   */
-  @SuppressWarnings("unchecked")
-  private void setupAddPointStorage() throws Exception {
-    PowerMockito.mockStatic(IncomingDataPoints.class);   
-    final byte[] row = new byte[] { 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1}; 
-    PowerMockito.doAnswer(
-        new Answer<byte[]>() {
-          public byte[] answer(final InvocationOnMock unused) 
-            throws Exception {
-            return row;
-          }
-        }
-    ).when(IncomingDataPoints.class, "rowKeyTemplate", (TSDB)any(), anyString(), 
-        (Map<String, String>)any());
+    Map<String, String> tags = new HashMap<String, String>();
+    tags.put("host", "web01");
+    bdp = new BatchedDataPoints(tsdb, "foo", tags);
   }
 
   @Test
-  public void timestamp() throws Exception {
-    setupAddPointStorage();
-    Map<String, String> tags = new HashMap<String, String>();
-    tags.put("host", "web01");
-    BatchedDataPoints bdp = new BatchedDataPoints(tsdb, "foo", tags);
+  public void timestamp() {
     bdp.addPoint(1388534400L, 1);
     bdp.addPoint(1388534400500L, 2);
     bdp.addPoint(1388534400750L, 2);
@@ -106,52 +105,86 @@ public class TestBatchedDataPoints {
     assertEquals(1388534400500L, bdp.timestamp(1));
     assertEquals(1388534400750L, bdp.timestamp(2));
   }
-  
+
   @Test
-  public void isInteger() throws Exception {
-    setupAddPointStorage();
-    Map<String, String> tags = new HashMap<String, String>();
-    tags.put("host", "web01");
-    BatchedDataPoints bdp = new BatchedDataPoints(tsdb, "foo", tags);
+  public void isInteger() {
     bdp.addPoint(1388534400L, 1);
     bdp.addPoint(1388534400500L, Short.MIN_VALUE);
     bdp.addPoint(1388534401L, Integer.MIN_VALUE);
     bdp.addPoint(1388534401750L, 2.0f);
-    
+
     assertTrue(bdp.isInteger(0));
     assertTrue(bdp.isInteger(1));
     assertTrue(bdp.isInteger(2));
     assertFalse(bdp.isInteger(3));
   }
-  
+
   @Test
-  public void longValue() throws Exception {
-    setupAddPointStorage();
-    Map<String, String> tags = new HashMap<String, String>();
-    tags.put("host", "web01");
-    BatchedDataPoints bdp = new BatchedDataPoints(tsdb, "foo", tags);
+  public void longValue() {
     bdp.addPoint(1388534400L, 1);
     bdp.addPoint(1388534400500L, Short.MIN_VALUE);
     bdp.addPoint(1388534401L, Integer.MIN_VALUE);
     bdp.addPoint(1388534401750L, 2.0f);
-    
+
     assertEquals(1, bdp.longValue(0));
     assertEquals(Short.MIN_VALUE, bdp.longValue(1));
     assertEquals(Integer.MIN_VALUE, bdp.longValue(2));
   }
-  
+
   @Test
-  public void doubleValue() throws Exception {
-    setupAddPointStorage();
-    Map<String, String> tags = new HashMap<String, String>();
-    tags.put("host", "web01");
-    BatchedDataPoints bdp = new BatchedDataPoints(tsdb, "foo", tags);
+  public void doubleValue() {
     bdp.addPoint(1388534400L, 1);
     bdp.addPoint(1388534400500L, Short.MIN_VALUE);
     bdp.addPoint(1388534401L, Integer.MIN_VALUE);
     bdp.addPoint(1388534401750L, 2.0f);
-    
+
     Assert.assertEquals(2.0f, bdp.doubleValue(3), 0.00001f);
   }
 
+  @Test
+  public void inBounds() {
+    long timestamp = 1388534400L;
+    long base_time = (timestamp - (timestamp % Const.MAX_TIMESPAN));
+
+    // test that the limit values are fine
+    bdp.addPoint(base_time, 2);
+    bdp.addPoint(base_time + Const.MAX_TIMESPAN - 1, 3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void addPrevTimestamp() {
+    bdp.addPoint(1388534400L, 1);
+    bdp.addPoint(1388534350L, 2);
+  }
+
+  @Test(expected = IllegalDataException.class)
+  public void outOfBounds() {
+    long timestamp = 1388534400L;
+    long base_time = (timestamp - (timestamp % Const.MAX_TIMESPAN));
+
+    bdp.addPoint(timestamp, 1); // this set's the batch's baseTime
+
+    // outside the limits addPoint throws an IllegalArgumentException
+    bdp.addPoint(base_time + Const.MAX_TIMESPAN, 2);
+  }
+
+  @Test
+  public void fullLoad() {
+    long timestamp = 1388534400L;
+    long base_time = (timestamp - (timestamp % Const.MAX_TIMESPAN));
+
+    for (int i = 0; i < Const.MAX_TIMESPAN; i++) {
+      bdp.addPoint(base_time + i, i);
+    }
+  }
+
+  @Test
+  public void fullLoadMs() {
+    long timestamp = 1388534400L;
+    long base_time = (timestamp - (timestamp % Const.MAX_TIMESPAN));
+
+    for (long i = 0; i < Const.MAX_TIMESPAN * 1000; i++) {
+      bdp.addPoint(base_time * 1000 + i, i);
+    }
+  }
 }
